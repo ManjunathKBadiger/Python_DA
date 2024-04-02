2b127887969b98673b6ba62cebb5ba32b401737b
ManjunathKBadiger <manjubadiger95@gmail.com>
2024-04-02 08:37:34 +0530

    Basics Python

---
+++ Basics/data_types.py
@@ -0,0 +1,19 @@
+# int
+a = 10
+print(a)
+print(type(a)) # to check the data type..
+
+# float
+
+b = 10.5
+print(b)
+print(type(b)) 
+
+c = 'Hello Python'
+print(c)
+print(type(c))
+
+s = [3, 4, 5]
+print(s)
+print(type(s))
+
